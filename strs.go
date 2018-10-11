156a8ca9892800dd987720ae48b1e066af67f0fe
Pan Dengyang <dengyang.pan@gmail.com>
2018-10-11 13:35:59 +0000

    [feature] add GetRandomString

---
+++ strs.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"time"
+	"math/rand"
+)
+
+func GetRandomString(length int) string {
+	bytes := []byte("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < length; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+
+	return string(result)
+}
