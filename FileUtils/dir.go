bf711239f06618dc1c1405686fae07314575a5fa
Pan Dengyang <dengyang.pan@aliyun.com>
2020-07-21 10:16:37 +0800

    [feature] MkdirAllByDate 返回生成的路径名

--- FileUtils/dir.go
+++ FileUtils/dir.go
@@ -6,6 +6,9 @@ import (
 	"time"
 )
 
-func MkdirAllByDate(parentDir string) (err error) {
-	return os.MkdirAll(filepath.Join(parentDir, time.Now().Format("2006-01-02")), os.ModePerm)
+func MkdirAllByDate(parentPath string) (path string, err error) {
+	path = filepath.Join(parentPath, time.Now().Format("2006-01-02"))
+	err = os.MkdirAll(path, os.ModePerm)
+
+	return path, err
 }
